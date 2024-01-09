9879f7664f54bba3f8771707a7aad9eca8b46a52
Name <email@example.ru>
2024-01-10 01:06:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string name = "Jane";
+byte age = 27;
+bool haveIAPet = true;
+double shoeSize = 37.5;
+
+Console.WriteLine("Как вас зовут?");
+Console.WriteLine("My name is " + name);
+Console.WriteLine("Сколько вам лет?");
+Console.WriteLine("My age is " + age);
+Console.WriteLine("Если ли у вас питомец?");
+Console.WriteLine("Do I have a pet? " + haveIAPet);
+Console.WriteLine("Какой у вас размер ноги?");
+Console.WriteLine("My shoe size is " + shoeSize);
